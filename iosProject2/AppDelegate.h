551670da7322ccb4609c8b05f0e4b5f71181ad7d
zhufeng <358244144@qq.com>
2018-06-09 09:36:01 -0700

    项目的初始化的东西导入

---
+++ iosProject2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosProject2
+//
+//  Created by bailing on 2018/6/9.
+//  Copyright © 2018年 zhufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
